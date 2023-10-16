2a2c102cff799969fd26aaf9c0161a3c4fde08b0
anjosoff <anjosgui12@gmail.com>
2023-10-16 19:15:24 -0300

    Feat: Adicionando a rota de Users e criando autenticação com JWT

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,48 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import cors from '@fastify/cors'
 import { appRoutes } from './routes'
+import userRoutes from "./modules/user/user.route";
+import { userSchemas } from "./modules/user/user.schema";
+import fjwt, { JWT } from "@fastify/jwt";
 
-const app = fastify()
+export const app = fastify();
+
+declare module "fastify" {
+    export interface FastifyInstance{
+        authenticate:any
+    }
+}
+
 
 app.register(cors)
 app.register(appRoutes)
+
+app.register(fjwt, {
+    secret: "mysecret",
+  });
+
+  app.decorate(
+    "authenticate",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        await request.jwtVerify();
+      } catch (e) {
+        return reply.send(e);
+      }
+    }
+  );
+
+for(const schema of userSchemas){
+    app.addSchema(schema)
+}
+app.register(userRoutes, {prefix:"/users"})
+
+//a
 
 app.listen({
     port: 3333,
 }).then(() => {
     console.log('HTTP Server Running!')
 })
+
+export default app;
